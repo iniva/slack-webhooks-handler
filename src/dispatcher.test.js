d9e3956adfbc6e86fd83823994fb08f9f9d03501
Tony <tonyblast@gmail.com>
2019-05-02 20:12:53 -0300

    [enhance-dispatcher] test: Updated dispatcher tests

--- src/dispatcher.test.js
+++ src/dispatcher.test.js
@@ -6,6 +6,8 @@ import Dispatcher from './dispatcher';
 
 jest.mock('@slack/webhook');
 
+const mockConfig = { url: 'https://url.test', botName: 'test bot' };
+
 describe('Slack Dispatcher', () => {
     it('should throw if Slack Webhook URL is not defined', () => {
         expect(() => {
@@ -13,8 +15,14 @@ describe('Slack Dispatcher', () => {
         }).toThrowError(/Slack Webhook URL is required/);
     });
 
+    it('should throw if not name is not defined', () => {
+        expect(() => {
+            const dispatcher = new Dispatcher({ url: 'https://url.test' }); // eslint-disable-line no-unused-vars
+        }).toThrowError(/bot name is required/);
+    });
+
     it('should return a Dispatcher instance', () => {
-        const dispatcher = new Dispatcher({ url: 'https://url.test' });
+        const dispatcher = new Dispatcher(mockConfig);
         const expectedProperties = ['webhook', 'username', 'text', 'attachments'];
 
         expect(dispatcher).toBeInstanceOf(Dispatcher);
@@ -23,7 +31,7 @@ describe('Slack Dispatcher', () => {
     });
 
     it('should throw when required variables are not set', async() => {
-        const dispatcher = new Dispatcher({ url: 'https://url.test' });
+        const dispatcher = new Dispatcher(mockConfig);
 
         try {
             await dispatcher.send();
@@ -33,8 +41,68 @@ describe('Slack Dispatcher', () => {
         }
     });
 
-    it('should set the attachments', async() => {
-        const dispatcher = new Dispatcher({ url: 'https://url.test' });
+    it('should set the text to an empty string if called with undefined param', () => {
+        const dispatcher = new Dispatcher(mockConfig);
+        dispatcher.setMessage();
+
+        expect(dispatcher.text).toEqual('');
+    });
+
+    it('should set the text', () => {
+        const dispatcher = new Dispatcher(mockConfig);
+        const message = 'test message';
+
+        dispatcher.setMessage(message);
+
+        expect(dispatcher.text).toEqual(message);
+    });
+
+    it('should set the channel to an empty string if called with undefined param', () => {
+        const dispatcher = new Dispatcher(mockConfig);
+        dispatcher.setChannel();
+
+        expect(dispatcher.channel).toEqual('');
+    });
+
+    it('should set the channel', () => {
+        const dispatcher = new Dispatcher(mockConfig);
+        const channel = '#test-message';
+
+        dispatcher.setMessage('test message');
+        dispatcher.setChannel(channel);
+
+        const message = {
+            username: dispatcher.username,
+            text: dispatcher.text,
+            attachments: dispatcher.attachments,
+            channel: dispatcher.channel
+        };
+
+        expect(dispatcher.channel).toEqual(channel);
+
+        dispatcher.send();
+
+        expect(dispatcher.webhook.send).toHaveBeenCalledWith(message);
+    });
+
+    it('should not set the channel in the message', () => {
+        const dispatcher = new Dispatcher(mockConfig);
+
+        dispatcher.setMessage('test');
+
+        const message = {
+            username: dispatcher.username,
+            text: dispatcher.text,
+            attachments: dispatcher.attachments
+        };
+
+        dispatcher.send();
+
+        expect(dispatcher.webhook.send).toHaveBeenCalledWith(message);
+    });
+
+    it('should set the attachments', () => {
+        const dispatcher = new Dispatcher(mockConfig);
         const attachments = [{
             field: 'value'
         }];
@@ -45,9 +113,10 @@ describe('Slack Dispatcher', () => {
     });
 
     it('should call the IncomingWebhook send function', () => {
-        const dispatcher = new Dispatcher({ url: 'https://url.test' });
+        const dispatcher = new Dispatcher(mockConfig);
 
         dispatcher.text = 'Test message';
+
         const response = dispatcher.send();
 
         expect(dispatcher.webhook.send).toHaveBeenCalledTimes(1);
